4f253a66b18eb3ddefd57fb7a1323db2099e4a8c
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-08-19 15:26:41 +0530

    Adding animation, hover state

--- components/FaqBoxTitle.js
+++ components/FaqBoxTitle.js
@@ -1,13 +1,7 @@
 import React, { Fragment, useState } from "react";
 import FaqBoxContent from "./FaqBoxContent";
 import classes from "./FaqBoxTitle.module.css";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import AddIcon from "@mui/icons-material/Add";
-import Layout from "./UI/Layout";
+import { motion } from "framer-motion";
 
 function FaqBoxTitle(props) {
   const [visibility, setVisibility] = useState(false);
@@ -17,30 +11,30 @@ function FaqBoxTitle(props) {
 
   return (
     <Fragment>
-      <div className={`${classes.titleFinal}`} onClick={toggleHandler}>
-        <p className={classes.title}>{props.title}</p>
-        <div className={classes.buttonAlign}>
-          <button className={classes.button}>
-            {props.visibility ? "-" : "+"}
-          </button>
-        </div>
-      </div>
-      <div className={classes.rel}>
-        {visibility && <FaqBoxContent content={props.content} />}
-      </div>
-      {/* <Accordion>
-        <AccordionSummary
-          className={classes.title}
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
+      <motion.div
+        layout
+        transition={{ layout: { duration: 0.5, type: "spring" } }}
+      >
+        <div
+          className={`${classes.titleFinal} ${
+            visibility ? classes.titleFinalActive : ""
+          }`}
+          onClick={toggleHandler}
         >
-          <Typography className={classes.title}>{props.title}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>{props.content}</Typography>
-        </AccordionDetails>
-      </Accordion> */}
+          {/* motion.h1 */}
+          <motion.p layout="fixed" className={classes.title}>
+            {props.title}
+          </motion.p>
+          <motion.div layout="fixed" className={classes.buttonAlign}>
+            <button className={classes.button}>
+              {props.visibility ? "-" : "+"}
+            </button>
+          </motion.div>
+        </div>
+        <div className={classes.rel}>
+          {visibility && <FaqBoxContent content={props.content} />}
+        </div>
+      </motion.div>
     </Fragment>
   );
 }
--- components/UI/Layout.js
+++ components/UI/Layout.js
@@ -2,7 +2,9 @@ import React from "react";
 import classes from "./Layout.module.css";
 
 function Layout(props) {
-  return <div className={classes.layout}>{props.children}</div>;
+  return (
+    <div className={`${classes.layout} ${classes.bgImg}`}>{props.children}</div>
+  );
 }
 
 export default Layout;
